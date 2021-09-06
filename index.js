e05702f0ead3b4164a128a315b01c2bfd57d00e4
Emmanuel Mwinama <bit15-emwinama@poly.ac.mw>
2021-09-06 17:51:03 +0200

    working template

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+const app = express();
+const conn = require('mysql');
+const port = process.env.PORT || 8000;
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res)=>{
+    res.render('home');
+});
+
+app.listen(port, ()=>{
+    console.log(`Server stsrted at http://localhost:${port}`);
+});
