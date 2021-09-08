2fca68f0836591f8d68a4a23e66a56cad44d7e02
Emmanuel Mwinama <bit15-emwinama@poly.ac.mw>
2021-09-08 11:01:26 +0200

    Tried using HTML to Jade extention

--- index.js
+++ index.js
@@ -5,10 +5,18 @@ const port = process.env.PORT || 8000;
 
 app.set('view engine', 'pug');
 
-app.get('/', (req, res)=>{
-    res.render('index');
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-app.listen(port, ()=>{
-    console.log(`Server stsrted at http://localhost:${port}`);
+app.get('/customer', (req, res) => {
+  res.render('form');
+});
+
+app.post('/addcustomer', (req, res) => {
+  res.render('index');
+});
+
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
 });
