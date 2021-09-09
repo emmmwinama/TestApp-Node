74f1d6df91898c77e8f5f8d42e79e74f287d46e5
Emmanuel Mwinama <bit15-emwinama@poly.ac.mw>
2021-09-09 10:23:53 +0200

    Succesfully submit text data to the database

--- index.js
+++ index.js
@@ -1,20 +1,51 @@
-var express = require('express');
-const app = express();
-const conn = require('mysql');
+const mysql = require('mysql');
+const express = require('express');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 8000;
 
+const app = express();
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-  res.render('index');
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'bankapp',
+  port: '3306',
 });
 
-app.get('/customer', (req, res) => {
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/add-customer', (req, res) => {
   res.render('form');
 });
 
-app.post('/addcustomer', (req, res) => {
-  res.render('index');
+// API endpoint to render HTML file
+app.get('/', (req, res) => {
+  res.status(200).render('index');
+});
+
+app.post('/customer', (req, res) => {
+  var query = 'INSERT INTO customer(Fullname, Age, Gender, EmployeeStat, idNumber) VALUES ?';
+  var values = [
+    [
+      req.body.name,
+      req.body.age,
+      req.body.gender,
+      req.body.status,
+      req.body.idnumber,
+    ],
+  ];
+  conn.connect((err) => {
+    if (err) throw err;
+    console.log('Connected');
+    conn.query(query, [values], (err, result) => {
+      if (err) throw err;
+      console.log('Number of Affected Rows: ' + result.affectedRows);
+    });
+  });
+  console.log(values);
+  res.redirect(303, '/');
 });
 
 app.listen(port, () => {
